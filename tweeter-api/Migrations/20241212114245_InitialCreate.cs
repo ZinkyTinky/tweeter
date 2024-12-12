08ca0449bb741557b57931467e953b16d36bc503
ZinkyTinky <zian@tsale.co.za>
2024-12-12 13:43:43 +0200

    Finished all of the API with database

---
+++ tweeter-api/Migrations/20241212114245_InitialCreate.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace final_project.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    TweeterId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Bio = table.Column<string>(type: "TEXT", nullable: false),
+                    ProfilePicture = table.Column<string>(type: "TEXT", nullable: true),
+                    following = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.TweeterId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tweet",
+                columns: table => new
+                {
+                    TweetId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    editedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    TweetBody = table.Column<string>(type: "TEXT", nullable: false),
+                    TweeterId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tweet", x => x.TweetId);
+                    table.ForeignKey(
+                        name: "FK_Tweet_Users_TweeterId",
+                        column: x => x.TweeterId,
+                        principalTable: "Users",
+                        principalColumn: "TweeterId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "TweeterId", "Bio", "Email", "FirstName", "LastName", "Password", "ProfilePicture", "Username", "following" },
+                values: new object[] { 1, "Honestly just a chill guy", "zian@tsale.co.za", "Zian", "Jacobsen", "Password1!", "https://i.imgur.com/1.jpg", "zianjacobsen", null });
+
+            migrationBuilder.InsertData(
+                table: "Tweet",
+                columns: new[] { "TweetId", "Title", "TweetBody", "TweeterId", "createdAt", "editedAt" },
+                values: new object[] { 1, "Cappa lover", "I'm just a chill guy who loves cappuccinos", 1, null, null });
+
+            migrationBuilder.InsertData(
+                table: "Tweet",
+                columns: new[] { "TweetId", "Title", "TweetBody", "TweeterId", "createdAt", "editedAt" },
+                values: new object[] { 2, "HC lover", "I'm just a chill guy who loves Hot chocolates", 1, null, null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tweet_TweeterId",
+                table: "Tweet",
+                column: "TweeterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tweet");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- tweeter-api/Migrations/TweeterDbContext.cs
+++ tweeter-api/Migrations/TweeterDbContext.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using tweeter_api.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,38 @@ public class TweeterDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.TweeterId);
+            entity.Property(e => e.Email).IsRequired();
+            entity.HasIndex(x => x.Email).IsUnique();
+            entity.Property(e => e.Password).IsRequired();
+            entity.Property(e => e.Username).IsRequired();
+            entity.Property(e => e.Bio).IsRequired();
+            entity.Property(e => e.ProfilePicture);
+
+            // Configure `following` to be stored as JSON
+            entity.Property(e => e.following)
+                .HasConversion(
+                    v => JsonSerializer.Serialize(v, new JsonSerializerOptions { WriteIndented = false }),
+                    v => JsonSerializer.Deserialize<string[]>(v, new JsonSerializerOptions { PropertyNameCaseInsensitive = true })
+                );
+        });
+
+        modelBuilder.Entity<User>().HasData(
+            new User
+            {
+                TweeterId = 1,
+                FirstName = "Zian",
+                LastName = "Jacobsen",
+                Email = "zian@tsale.co.za",
+                Password = "Password1!",
+                Username = "zianjacobsen",
+                Bio = "Honestly just a chill guy",
+                ProfilePicture = "https://i.imgur.com/1.jpg",
+            }
+        );
+
         modelBuilder.Entity<Tweet>(entity =>
         {
             entity.HasKey(e => e.TweetId);
@@ -27,13 +60,15 @@ public class TweeterDbContext : DbContext
         });
 
         modelBuilder.Entity<Tweet>().HasData(
-            new Tweet { 
+            new Tweet
+            {
                 TweetId = 1,
                 Title = "Cappa lover",
-                TweetBody = "I'm just a chill guy who loves capppuccinos",
+                TweetBody = "I'm just a chill guy who loves cappuccinos",
                 TweeterId = 1
             },
-            new Tweet { 
+            new Tweet
+            {
                 TweetId = 2,
                 Title = "HC lover",
                 TweetBody = "I'm just a chill guy who loves Hot chocolates",
@@ -41,16 +76,6 @@ public class TweeterDbContext : DbContext
             }
         );
 
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.HasKey(e => e.TweeterId);
-            entity.Property(e => e.Email).IsRequired();
-            entity.HasIndex(x => x.Email).IsUnique();
-            entity.Property(e => e.Password).IsRequired();
-            entity.Property(e => e.Username).IsRequired();
-            entity.Property(e => e.Bio).IsRequired();
-            entity.Property(e => e.ProfilePicture);
-            entity.Property(e => e.following);
-        });
+        
     }
 }
