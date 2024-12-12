cfaa5d3b5550df75c04e4c35e305aa1c51e71f21
ZinkyTinky <zian@tsale.co.za>
2024-12-12 13:21:37 +0200

    Finished all controllers

---
+++ tweeter-api/Controllers/TweetController.cs
@@ -0,0 +1,70 @@
+using tweeter_api.Models;
+using tweeter_api.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tweeter_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TweetController : ControllerBase 
+    {
+        private readonly ILogger<TweetController> _logger;
+        private readonly ITweetRepository _tweetRepository;
+
+        public TweetController(ILogger<TweetController> logger, ITweetRepository repository)
+        {
+            _logger = logger;
+            _tweetRepository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Tweet>> GetTweet() 
+        {
+            return Ok(_tweetRepository.GetAllTweets());
+        }
+
+        [HttpGet]
+        [Route("{tweetId:int}")]
+        public ActionResult<Tweet> GetTweetByTitle(string title) 
+        {
+            var tweet = _tweetRepository.GetTweetByTitle(title);
+            if (title == null) {
+                return NotFound();
+            }
+            return Ok(title);
+        }
+
+        [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<Tweet> CreateTweet(Tweet tweet) 
+        {
+            if (!ModelState.IsValid || tweet == null) {
+                return BadRequest();
+            }
+            var newTweet = _tweetRepository.CreateTweet(tweet);
+            return Created(nameof(GetTweetByTitle), newTweet);
+        }
+
+        [HttpPut]
+        [Route("{tweetId:int}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<Tweet> UpdateTweet(Tweet tweet) 
+        {
+            if (!ModelState.IsValid || tweet == null) {
+                return BadRequest();
+            }
+            return Ok(_tweetRepository.UpdateTweet(tweet));
+        }
+
+        [HttpDelete]
+        [Route("{tweetId:int}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult DeleteTweet(int tweetId) 
+        {
+            _tweetRepository.DeleteTweetById(tweetId); 
+            return NoContent();
+        }
+    }
+}
